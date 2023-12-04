424f422825fc23ce633c40e1c0b8341b54a0dd31
Bagus Tri Atmojo <bbagustrm@gmail.com>
2023-12-04 13:05:47 +0700

    add file

--- src/Node.java
+++ src/Node.java
@@ -1,15 +1,15 @@
 import java.util.*;
 
 public class Node<T> {
-    private T data;
+    private T nama;
     private Map<Node<T>, Integer> adjacentNodes = new HashMap<>();
 
     public Node(T data) {
-        this.data = data;
+        this.nama = data;
     }
 
     public T getData() {
-        return data;
+        return nama;
     }
 
     public void addAdjacentNode(Node<T> node, int weight) {
