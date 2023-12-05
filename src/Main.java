23de5044bc345bcb236165cbc3570835f9265b08
Bagus Tri Atmojo <bbagustrm@gmail.com>
2023-12-05 07:25:14 +0700

    add more struc data

--- src/Main.java
+++ src/Main.java
@@ -3,12 +3,12 @@ import java.util.List;
 public class Main {
     public static void main(String[] args) {
 
-        Node<String> nodeA = new Node<>("JNE1");
-        Node<String> nodeB = new Node<>("JNE2");
-        Node<String> nodeC = new Node<>("JNE3");
-        Node<String> nodeD = new Node<>("JNE4");
-        Node<String> nodeE = new Node<>("JNE5");
-        Node<String> nodeF = new Node<>("JNE6");
+        Node<String> nodeA = new Node<>("JNE1", "Jl.Mangkudipuro No 41", true);
+        Node<String> nodeB = new Node<>("JNE2", "Jl.Mulawarman No 23", true);
+        Node<String> nodeC = new Node<>("JNE3", "Jl.Messi No 7", true);
+        Node<String> nodeD = new Node<>("JNE4", "Jl.Diponegoro No 69", true);
+        Node<String> nodeE = new Node<>("JNE5", "Jl.Tirto Agung No 45", true);
+        Node<String> nodeF = new Node<>("JNE6", "Jl.Sudirman No 82", true);
 
         nodeA.addAdjacentNode(nodeB, 2);
         nodeA.addAdjacentNode(nodeC, 4);
--- src/Node.java
+++ src/Node.java
@@ -2,10 +2,14 @@ import java.util.*;
 
 public class Node<T> {
     private T nama;
+    private String alamat;
+    private Boolean isOpen;
     private Map<Node<T>, Integer> adjacentNodes = new HashMap<>();
 
-    public Node(T data) {
+    public Node(T data, String alamat, Boolean isOpen) {
         this.nama = data;
+        this.alamat = alamat;
+        this.isOpen = isOpen;
     }
 
     public T getData() {
